8d1559dc0f6942b6a9b6d4895447026c398f0e95
shusaku009 <shusaku009@gmail.com>
2024-06-30 15:19:33 +0900

    update: main.goの処理を別のファイルに切り分ける

--- main.go
+++ main.go
@@ -1,87 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"runtime"
-	"time"
+	"go-app/packages"
 )
 
 func main() {
-	// for文
-	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
-	}
-	fmt.Println(sum)
-
-	// for文セミコロン省略
-	num := 1
-	for num < 1000 {
-		num += sum
-	}
-	fmt.Println(num)
-	fmt.Println(sqrt(2), sqrt(-4))
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 20),
-	)
-
-	fmt.Print("Go runs on ")
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
-	default:
-		fmt.Printf("%s. \n", os)
-	}
-
-	fmt.Print("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 1:
-		fmt.Println("Today.")
-	case today + 2:
-		fmt.Println("Tomorrow.")
-	default:
-		fmt.Println("Too far away.")
-	}
-
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon.")
-	default:
-		fmt.Println("Good evening.")
-	}
-
-	defer fmt.Println("world")
-	fmt.Println("hello")
-
-	fmt.Println("counting")
-
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("done")
-}
-
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
-	}
-	return fmt.Sprint(math.Sqrt(x))
-}
-
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
-	}
-	return lim
+	packages.Packages()
 }
