a939a269ea7ad5a99c133f06eaa6f0e4e1710309
shusaku009 <shusaku009@gmail.com>
2025-03-16 19:27:55 +0900

    add: Exercise: Errors

--- script/exercise-errors.go
+++ script/exercise-errors.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+var eps float64 = math.Nextafter(1, 2) - 1
 
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return x, ErrNegativeSqrt(x)
+	}
+
+	z := 1.0
+	prev_z := z
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		if math.Abs(z-prev_z) <= eps {
+			break
+		}
+		prev_z = z
+	}
+	return z, nil
+}
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
 func main() {
